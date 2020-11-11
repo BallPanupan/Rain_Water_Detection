a6557c911bfff9ad43165095a14af3f2cb494ebe
Panupan Tantiwacharapan <ball085573@hotmail.com>
2020-11-11 23:58:28 +0700

    Update Rain_controller.ino

--- Rain_controller.ino
+++ Rain_controller.ino
@@ -1,10 +1,13 @@
-int inPin = 2;  
+#include "WaterPump.h"
+int inPin = 2;
 
-int analogPin = A0;  
+int analogPin = A0;
 int val2 = 0;
 
-int rain_val;      
-int relay_pin = 15; 
+int rain_val;
+int relay_pin = 15;
+//int relay_pin2 = 14; // water pump
+long count_time = 0; //36000000
 
 int led_pin1 = 7;
 int led_pin2 = 8;
@@ -24,33 +27,38 @@ long time_btn3 = 28800000;// 480 minute | 8 hourst = 28800000
 
 void setup() {
   Serial.begin(9600);
-  pinMode(inPin, INPUT);   
-   
+  pinMode(inPin, INPUT);
+
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
-  
-  pinMode(relay_pin, OUTPUT);
 
+  pinMode(relay_pin, OUTPUT);
+  pinMode(relay_pin2, OUTPUT);
+  digitalWrite(relay_pin2, HIGH);
+  
   pinMode(led_pin1, OUTPUT);
   pinMode(led_pin2, OUTPUT);
   pinMode(led_pin3, OUTPUT);
 
   
+
+
 }
 
 void loop() {
   val2 = analogRead(analogPin);
   Serial.print("val = "); // พิมพ์ข้อมความส่งเข้าคอมพิวเตอร์ "val = "
   Serial.println(val2); // พิมพ์ค่าของตัวแปร val
-  
+
   buttonState1 = digitalRead(buttonPin1);
   buttonState2 = digitalRead(buttonPin2);
   buttonState3 = digitalRead(buttonPin3);
 
-  
+
   //เช็คฝน 1 = ไม่มีฝน | 0 = มีฝน
-  rain_val = digitalRead(inPin);   
+  rain_val = digitalRead(inPin);
+  Serial.print("Water Detec = ");
   Serial.println(rain_val);
 
   if (buttonState1 == HIGH) {
@@ -60,10 +68,10 @@ void loop() {
     pinMode(relay_pin, HIGH);
     Serial.println("3 ms");
     delay(time_btn1); // is 'time_btn1'
-  }else{
+  } else {
     digitalWrite(led_pin1, LOW);
     pinMode(relay_pin, LOW);
-    }
+  }
   if (buttonState2 == HIGH) {
     // turn LED on:
     Serial.println("_________ on 2");
@@ -71,11 +79,11 @@ void loop() {
     pinMode(relay_pin, HIGH);
     Serial.println("60 minute");
     delay(time_btn2); // is 'time_btn2'
-    
-  }else{
+
+  } else {
     digitalWrite(led_pin2, LOW);
     pinMode(relay_pin, LOW);
-    }
+  }
   if (buttonState3 == HIGH) {
     // turn LED on:
     Serial.println("_________ on 3");
@@ -83,19 +91,28 @@ void loop() {
     pinMode(relay_pin, HIGH);
     Serial.println("3 Hours");
     delay(time_btn3);// is 'time_btn3'
-  }else{
+  } else {
     digitalWrite(led_pin3, LOW);
     pinMode(relay_pin, LOW);
-    }
+  }
 
-  if(rain_val == 1){ //is water_Detection
+  if (rain_val == 1) { //is water_Detection
     pinMode(relay_pin, LOW);
-    }
-  else{
+  }
+  else {
     pinMode(relay_pin, HIGH);
     delay(water_Detection);
-    }
-    
+  }
+
   delay(200);
+
+  Serial.print("Time = ");
+  Serial.println(count_time);
+  if (count_time >= 36000000) { //36000000
+    count_time = 0;
+    water_on();
+  }else{
+    count_time++;
+    }
+
 }
-  
